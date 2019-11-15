c9ffd2974430d9e6ba470ab6613c2219d78722cd
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-11-15 13:33:08 -0600

    added getmovie, postmovie, patchmovie

--- src/api.js
+++ src/api.js
@@ -1,6 +1,28 @@
-module.exports = {
-  getMovies: () => {
-    return fetch('/api/movies')
-      .then(response => response.json());
-  }
-};
+ module.exports = {
+      getMovies: () => {
+        return fetch('/api/movies')
+            .then(response => response.json());
+      },
+      getMovie : (id) => {
+        return fetch(`/api/movies/${id}`)
+            .then(resp => resp.json());
+      },
+      postMovie : (book) => {
+        return fetch('api/movies', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(book),
+        })
+      },
+      patchMovie : (book, id) => {
+        return fetch(`api/movies/${id}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(book),
+        })
+      },
+    };
